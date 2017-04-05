ba2e746089a7d54b28c105b3c10a634f0d26cd02
handav <hannahdavis0220@gmail.com>
2017-04-05 16:08:25 +0200

    updating gitignore

--- markovText/twitter_archive/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 975,
-  "created_at" : "2017-02-22 22:24:37 +0000",
-  "lang" : "en"
-}
--- markovTwitterBot/twitter_archive/data/js/user_details.js
+++
@@ -1,8 +0,0 @@
-var user_details =  {
-  "screen_name" : "ahandvanish",
-  "location" : "nomad\/NYC\/RI",
-  "full_name" : "Hannah Davis",
-  "bio" : "Generating music from books\/text\/emotion. Into sonification, NLP, ML, weird mappings, new-idea vocabularies, unique datasets, possible futures, microbiomes.",
-  "id" : "191100226",
-  "created_at" : "2010-09-15 16:17:35 +0000"
-}
