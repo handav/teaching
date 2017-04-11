8a628361080bb591a8f594134753952ab2f83cb4
handav <hannahdavis0220@gmail.com>
2017-04-11 16:49:14 +0200

    updating

--- tracery-project/index.js
+++ tracery-project/index.js
@@ -1,32 +1,33 @@
 var tracery = require('tracery-grammar');
 
 //SIMPLE EXAMPLE
-// var grammar = tracery.createGrammar({
-//     'character': ['Karl', 'Aida', 'Hans'],
-//     'action': ['walk', 'stroll', 'meander'],
-//     'place': ['office', 'bank', 'court'],
-//     'object': ['letter', 'paper', 'bribe'],
-//     'origin': ['#character.capitalize# #action.ed# to the #place# for #object.a#.']
-// });
-
-//COMPLEX EXAMPLE
 var grammar = tracery.createGrammar({
     'character': ['Karl', 'Aida', 'Hans'],
+    'action': ['walk', 'stroll', 'meander'],
+    'food': ['apple', 'banana', 'pineapple'],
     'place': ['office', 'bank', 'court'],
     'object': ['letter', 'paper', 'bribe'],
-    'setPronouns': [
-        '[they:they][them:them][their:their][theirs:theirs]',
-        '[they:she][them:her][their:her][theirs:hers]',
-        '[they:he][them:him][their:his][theirs:his]'
-    ],
-    'setJob': [
-        '[job:lawyer][actions:argued in court,filed some paperwork]',
-        '[job:inspector][actions:talked with the lawyer,conducted meetings]',
-        '[job:officer][actions:arrested people,stood in the courtroom]'
-    ],
-    'story': ['#protagonist# the #job# went to the #place# every day. Usually #they# #actions#. Then #they# picked up #their# #object#.'],
-    'origin': ['#[#setPronouns#][#setJob#][protagonist:#character#]story#']
+    'origin': ['#character.capitalize# #action.ed# to the #place# for #object.a#.']
 });
+
+//COMPLEX EXAMPLE
+// var grammar = tracery.createGrammar({
+//     'character': ['Karl', 'Aida', 'Hans'],
+//     'place': ['office', 'bank', 'court'],
+//     'object': ['letter', 'paper', 'bribe'],
+//     'setPronouns': [
+//         '[they:they][them:them][their:their][theirs:theirs]',
+//         '[they:she][them:her][their:her][theirs:hers]',
+//         '[they:he][them:him][their:his][theirs:his]'
+//     ],
+//     'setJob': [
+//         '[job:lawyer][actions:argued in court,filed some paperwork]',
+//         '[job:inspector][actions:talked with the lawyer,conducted meetings]',
+//         '[job:officer][actions:arrested people,stood in the courtroom]'
+//     ],
+//     'story': ['#protagonist# the #job# went to the #place# every day. Usually #they# #actions#. Then #they# picked up #their# #object#.'],
+//     'origin': ['#[#setPronouns#][#setJob#][protagonist:#character#]story#']
+// });
 
 grammar.addModifiers(tracery.baseEngModifiers);
 
